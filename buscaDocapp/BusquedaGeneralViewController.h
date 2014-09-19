ab23cf9118a10227b469c4e25b4cfe9bebf95dcb
karina0311 <nancyramirez@iMac-de-Nancy.local>
2014-09-19 02:37:26 -0500

    commit

---
+++ buscaDocapp/BusquedaGeneralViewController.h
@@ -0,0 +1,32 @@
+//
+//  BusquedaGeneralViewController.h
+//  buscaDocapp
+//
+//  Created by inf227al on 10/09/14.
+//  Copyright (c) 2014 tesis2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusquedaGeneralViewController : UIViewController <UITextFieldDelegate, UIPickerViewDelegate> {
+
+    NSMutableArray *especialidad;
+    UIPickerView *pickerespecialidad;
+    
+    NSMutableArray *distrito;
+    UIPickerView *pickerdistrito;
+    
+    NSMutableArray *seguro;
+    UIPickerView *pickerseguro;
+
+}
+
+
+@property (weak, nonatomic) IBOutlet UITextField *lblEspecialidad;
+@property (weak, nonatomic) IBOutlet UITextField *lblDistrito;
+@property (weak, nonatomic) IBOutlet UITextField *lblSeguro;
+@property (weak, nonatomic) IBOutlet UITextField *lblDia;
+
+
+
+@end
