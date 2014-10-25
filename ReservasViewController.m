366b58e5c38d28392119915e139bbbf088aa69bd
karina0311 <karina.arevalo@pucp.pe>
2014-10-25 17:58:28 -0500

    commit

--- ReservasViewController.m
+++ ReservasViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "ReservasViewController.h"
+#import "UIImageView+AFNetworking.h"
+#import "URLS json.h"
 
 @interface ReservasViewController ()
+
 
 @end
 
 @implementation ReservasViewController
+
+NSDictionary * respuesta;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,29 +31,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.iconCita.layer.cornerRadius = self.iconCita.frame.size.width / 2;
-    self.lblNombreDoctor.text= [NSString stringWithFormat:@"Dr. %@ %@ ", self.name, self.lastname];
-    self.lblEspecialidad.text= self.nombreespecialidad;
     
-    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-    [dateFormat setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss-zz:zz"];
-    
-    NSDateFormatter *dateFormat3 = [[NSDateFormatter alloc] init];
-    [dateFormat3 setDateFormat:@"HH:mm"];
-    
-    //NSDate *fecha = [dateFormat dateFromString:Date];
-    
-    NSDate *date = [dateFormat dateFromString:self.horainicio];
-    NSTimeInterval secondsInEightHours = 29 * 60 * 60;
-    NSDate *dateEightHoursAhead = [date dateByAddingTimeInterval:secondsInEightHours];
-    
-    
-    
-    NSString *horai = [dateFormat3 stringFromDate:dateEightHoursAhead];
+    [self ActualizaDatosClinica];
 
     
-    
 }
+
+- (IBAction)confirmarReserva:(id)sender {
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -56,15 +47,72 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void) ActualizaDatosClinica{
+    
+    NSDictionary *consulta = @{@"idclinic": self.idclinic};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    NSLog(@"%@", consulta);
+    
+    [manager POST:clinicaxid parameters:consulta success:^(AFHTTPRequestOperation *task, id responseObject) {
+        respuesta = responseObject;
+        NSLog(@"JSON: %@", respuesta);
+        
+        NSDictionary * diccionario2=  respuesta[@"clinic"];
+        NSString *clinicaname= diccionario2[@"name"];
+        NSString *direccion= diccionario2[@"address"];
+        NSString *telefono= diccionario2[@"phone"];
+        
+        
+        self.iconCita.layer.cornerRadius = self.iconCita.frame.size.width / 2;
+        self.iconCita.clipsToBounds = YES;
+        self.lblNombreDoctor.text= [NSString stringWithFormat:@"Dr. %@ %@ ", self.name, self.lastname];
+        self.lblEspecialidad.text= self.nombreespecialidad;
+        
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+        
+        NSDateFormatter *dateFormat2 = [[NSDateFormatter alloc] init];
+        [dateFormat2 setDateFormat:@"dd/MM/yyyy"];
+        
+        NSDateFormatter *dateFormat3 = [[NSDateFormatter alloc] init];
+        [dateFormat3 setDateFormat:@"HH:mm"];
+        
+        //NSDate *fecha = [dateFormat dateFromString:self.horainicio];
+        
+        //NSDate *date = [dateFormat dateFromString:fecha];
+        //NSTimeInterval secondsInEightHours = 29 * 60 * 60;
+        //NSDate *dateEightHoursAhead = [self.horainicio dateByAddingTimeInterval:secondsInEightHours];
+        
+        
+        NSString *fechasel =[dateFormat stringFromDate:self.fechaseleccionada];
+        //NSString *horai = [dateFormat3 stringFromDate:dateEightHoursAhead];
+        
+        
+        self.lblHora.text=self.horainicio;
+        self.lblFecha.text=fechasel;
+        self.lblNombreClinica.text=clinicaname;
+        self.lblDireccion.text=direccion;
+        self.lblTelefono.text=telefono;
+        
+    }
+          failure:^(AFHTTPRequestOperation *task, NSError *error) {
+              UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"No choco con el servidor"
+                                                                  message:[error localizedDescription]
+                                                                 delegate:nil
+                                                        cancelButtonTitle:@"Ok"
+                                                        otherButtonTitles:nil];
+              [alertView show];
+          }];
+    
+    
+    
+    
 }
-*/
+
+
+
 
 @end
--- buscaDocapp/URLS json.h
+++ buscaDocapp/URLS json.h
@@ -12,35 +12,35 @@
 
 //Cambiar la ip segun corresponda
 
-//ip de la cato: 10.101.33.0
+//ip de la cato: 10.101.32.184
 //ip de mi casa: 192.168.0.104
 
-#define listaespecialidades @"http://10.101.33.0:8080/tesis2Karina/services/getAllSpecialtiesJSON"
-#define docsxespecialidad @"http://10.101.33.0:8080/tesis2Karina/services/getAllDoctorsBySpecialtyJSON"
-#define listadistritos @"http://10.101.33.0:8080/tesis2Karina/services/getAllDistrictsJSON"
-#define listaseguros @"http://10.101.33.0:8080/tesis2Karina/services/getAllInsurancesJSON"
-#define listaclinicas @"http://10.101.33.0:8080/tesis2Karina/services/getAllClinicsJSON"
-#define docsxclinica @"http://10.101.33.0:8080/tesis2Karina/services/getAllDoctorsByClinicJSON"
-#define docsxdistrito @"http://10.101.33.0:8080/tesis2Karina/services/getAllDoctorsByDistrictJSON"
+#define listaespecialidades @"http://10.101.32.184:8080/tesis2Karina/services/getAllSpecialtiesJSON"
+#define docsxespecialidad @"http://10.101.32.184:8080/tesis2Karina/services/getAllDoctorsBySpecialtyJSON"
+#define listadistritos @"http://10.101.32.184:8080/tesis2Karina/services/getAllDistrictsJSON"
+#define listaseguros @"http://10.101.32.184:8080/tesis2Karina/services/getAllInsurancesJSON"
+#define listaclinicas @"http://10.101.32.184:8080/tesis2Karina/services/getAllClinicsJSON"
+#define docsxclinica @"http://10.101.32.184:8080/tesis2Karina/services/getAllDoctorsByClinicJSON"
+#define docsxdistrito @"http://10.101.32.184:8080/tesis2Karina/services/getAllDoctorsByDistrictJSON"
 
-#define docsbusqueda @"http://10.101.33.0:8080/tesis2Karina/services/getDoctorsBySearch"
+#define docsbusqueda @"http://10.101.32.184:8080/tesis2Karina/services/getDoctorsBySearch"
 
-#define clinicaxid @"http://10.101.33.0:8080/tesis2Karina/services/getClinicByIdJSON"
+#define clinicaxid @"http://10.101.32.184:8080/tesis2Karina/services/getClinicByIdJSON"
 
-#define horarioxdoc @"http://10.101.33.0:8080/tesis2Karina/services/getSchedulesByDoctorJSON"
+#define horarioxdoc @"http://10.101.32.184:8080/tesis2Karina/services/getSchedulesByDoctorJSON"
 
-#define bloquexhorario @"http://10.101.33.0:8080/tesis2Karina/services/getBlocksByDateSchedule"
+#define bloquexhorario @"http://10.101.32.184:8080/tesis2Karina/services/getBlocksByDateSchedule"
 
-#define guardaPaciente @"http://10.101.33.0:8080/tesis2Karina/services/savePatient"
+#define guardaPaciente @"http://10.101.32.184:8080/tesis2Karina/services/savePatient"
 
-#define guardaUser @"http://10.101.33.0:8080/tesis2Karina/services/saveUser"
+#define guardaUser @"http://10.101.32.184:8080/tesis2Karina/services/saveUser"
 
-#define guardaCita @"http://10.101.33.0:8080/tesis2Karina/services/saveAppoinment"
+#define guardaCita @"http://10.101.32.184:8080/tesis2Karina/services/saveAppoinment"
 
-#define sacaUltimoPaciente @"http://10.101.33.0:8080/tesis2Karina/services/getLastPatientJSON"
+#define sacaUltimoPaciente @"http://10.101.32.184:8080/tesis2Karina/services/getLastPatientJSON"
 
-#define login @"http://10.101.33.0:8080/tesis2Karina/services/Login"
+#define login @"http://10.101.32.184:8080/tesis2Karina/services/Login"
 
-#define guardaSegurosxPaciente @"http://10.101.33.0:8080/tesis2Karina/services/saveInsurancesPatient"
+#define guardaSegurosxPaciente @"http://10.101.32.184:8080/tesis2Karina/services/saveInsurancesPatient"
 
 @end
