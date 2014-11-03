490af63d1a52636fa4a589d5399e1b3c6bb2b73f
karina0311 <karina.arevalo@pucp.pe>
2014-11-03 17:56:22 -0500

    tableviewcell mi doctor

---
+++ buscaDocapp/MiDoctorTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MiDoctorTableViewCell.h
+//  buscaDocapp
+//
+//  Created by inf227al on 3/11/14.
+//  Copyright (c) 2014 tesis2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiDoctorTableViewCell : UITableViewCell
+
+@end
---
+++ buscaDocapp/MiDoctorTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MiDoctorTableViewCell.m
+//  buscaDocapp
+//
+//  Created by inf227al on 3/11/14.
+//  Copyright (c) 2014 tesis2. All rights reserved.
+//
+
+#import "MiDoctorTableViewCell.h"
+
+@implementation MiDoctorTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
